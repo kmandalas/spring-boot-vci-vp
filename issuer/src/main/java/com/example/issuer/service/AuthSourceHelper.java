e4576c265257f03665ccf96a2f2b857c52df474d
kmandalas <kmandalas@cognity.gr>
2025-05-02 19:42:33 +0300

    AuthSourceHelper test data

--- issuer/src/main/java/com/example/issuer/service/AuthSourceHelper.java
+++ issuer/src/main/java/com/example/issuer/service/AuthSourceHelper.java
@@ -22,17 +22,17 @@ public class AuthSourceHelper {
     public List<Disclosure> getDisclosableClaims(String username) {
         return switch (username) {
             case "testuser1" -> List.of(
-                    new Disclosure("credential_holder", "Kyriakos Mandalas"),
+                    new Disclosure("credential_holder", "Nikos Testopoulos"),
                     new Disclosure("nationality", "Greek \uD83C\uDDEC\uD83C\uDDF7"),
                     new Disclosure("competent_institution", "EOPYY")
             );
             case "testuser2" -> List.of(
-                    new Disclosure("credential_holder", "Alina Adminescu"),
+                    new Disclosure("credential_holder", "Alina Validescu"),
                     new Disclosure("nationality", "Romanian \uD83C\uDDF7\uD83C\uDDF4"),
                     new Disclosure("competent_institution", "CNAS")
             );
             case "testuser3" -> List.of(
-                    new Disclosure("credential_holder", "Tudor Iliev"),
+                    new Disclosure("credential_holder", "Tudor Testorov"),
                     new Disclosure("nationality", "Bulgarian \uD83C\uDDE7\uD83C\uDDEC"),
                     new Disclosure("competent_institution", "НЗОК")
             );
