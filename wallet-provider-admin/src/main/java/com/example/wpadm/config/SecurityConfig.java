364b75c71118c19e1edf9277fca71aaf79cc4953
kmandalas <kmandalas@cognity.gr>
2026-03-19 02:36:45 +0200

    Use delegating password encoder instead of BCrypt directly

--- wallet-provider-admin/src/main/java/com/example/wpadm/config/SecurityConfig.java
+++ wallet-provider-admin/src/main/java/com/example/wpadm/config/SecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.config.annotation.authorization.EnableMultiF
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.authority.FactorGrantedAuthority;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
@@ -28,7 +28,7 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     /**
